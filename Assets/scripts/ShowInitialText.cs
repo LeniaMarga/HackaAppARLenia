3ca7af69dfd5cf192923dc16d2e0d618d4d48215
Eleni Margariti <ekamarga@gmail.com>
2018-03-25 18:45:10 +0300

    changed

---
+++ Assets/scripts/ShowInitialText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowInitialText : MonoBehaviour
+{
+
+    private GUIStyle guiStyle = new GUIStyle(); //create a new variable
+
+ 
+
+   
+
+    private void OnGUI()
+    {
+        guiStyle.fontSize = 24; //change the font size
+       GUILayout.Label("Write your text here.", guiStyle);
+
+        string myString = "Σκαναρε τον Hacka!";
+        int textWidth = myString.Length * 10;
+        //10 is a constant which will obviously vary as you change the font or its size, 
+        //so you'll need a little of pacience to get a good value there
+
+        GUI.Label(new Rect(Screen.width / 2 - textWidth / 2, Screen.height / 2, textWidth, 40), myString, guiStyle);
+      
+    }
+
+
+}
