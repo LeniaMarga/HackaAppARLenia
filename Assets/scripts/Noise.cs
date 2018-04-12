9b437973f9ebb549fbe5799daedd7eb594a3721f
Eleni Margariti <ekamarga@gmail.com>
2018-04-12 10:32:23 +0300

    changed bubbles

---
+++ Assets/scripts/Noise.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noise : MonoBehaviour {
+
+
+    public AudioClip makeNoise1;
+    public AudioClip makeNoise2;
+    AudioSource sound1;
+    AudioSource sound2;
+    
+
+    // Use this for initialization
+    void Start () {
+        
+      
+	}
+	
+
+    void Noise1(){
+        sound1 = GetComponent<AudioSource>();
+        sound1.PlayOneShot(makeNoise1, 0.6f);
+
+    }
+
+    void Noise2()
+    {
+        sound2 = GetComponent<AudioSource>();
+        sound2.PlayOneShot(makeNoise2, 0.6f);
+
+    }
+}
--- Assets/scripts/ShowInitialText.cs
+++ Assets/scripts/ShowInitialText.cs
@@ -1,29 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+#if UNITY_EDITOR 
+using UnityEditor;
+#endif
 using UnityEngine;
+using Vuforia;
+using System.Linq;
 
 public class ShowInitialText : MonoBehaviour
 {
 
     private GUIStyle guiStyle = new GUIStyle(); //create a new variable
-
- 
-
-   
+    public Texture focusTexture;
+   //public Texture2D texture;
+  // public Rect position;
 
     private void OnGUI()
     {
-        guiStyle.fontSize = 24; //change the font size
-       GUILayout.Label("Write your text here.", guiStyle);
+        // guiStyle.fontSize = 24; //change the font size
+        // guiStyle.normal.background = texture;
 
-        string myString = "Σκαναρε τον Hacka!";
-        int textWidth = myString.Length * 10;
-        //10 is a constant which will obviously vary as you change the font or its size, 
-        //so you'll need a little of pacience to get a good value there
+        // string myString = "Σκαναρε τον Hacka!";
+        //  int textWidth = myString.Length * 10;
 
-        GUI.Label(new Rect(Screen.width / 2 - textWidth / 2, Screen.height / 2, textWidth, 40), myString, guiStyle);
-      
-    }
+
+        //GUI.Label(new Rect(Screen.width /2  - textWidth / 2, Screen.height/2 , textWidth, 40), myString, guiStyle);
+        //  EditorGUI.LabelField(position, GUIContent.none, guiStyle);
+
+
+
+        if (!GetComponent<TrackableList>().activeTrackables.Any())
+            {
+            //GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 120, 50), "Message goes here");
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), focusTexture, ScaleMode.StretchToFill);
+        }
+        else
+            {
+              
+            }
+
+
+            
+
+        }
+
 
 
 }
